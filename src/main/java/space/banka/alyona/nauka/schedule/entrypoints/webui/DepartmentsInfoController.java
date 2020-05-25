60d5c9554eba8a0fd2e758abf0aa2fec77c690df
Alyona Kovalyova <alyona+github@banka.space>
2020-05-25 19:48:59 +0300

    Create Employees and Departments Controllers

---
+++ src/main/java/space/banka/alyona/nauka/schedule/entrypoints/webui/DepartmentsInfoController.java
@@ -0,0 +1,20 @@
+package space.banka.alyona.nauka.schedule.entrypoints.webui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import space.banka.alyona.nauka.schedule.db.crud.DepartmentRepository;
+
+@Controller
+public class DepartmentsInfoController {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @RequestMapping("/departments")
+    String showEmployeesInfo(Model model) {
+        model.addAttribute("departments", departmentRepository.findAll());
+        return "departments_info";
+    }
+}
---
+++ src/main/java/space/banka/alyona/nauka/schedule/entrypoints/webui/EmployeesInfoController.java
@@ -0,0 +1,20 @@
+package space.banka.alyona.nauka.schedule.entrypoints.webui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import space.banka.alyona.nauka.schedule.db.crud.EmployeeRepository;
+
+@Controller
+public class EmployeesInfoController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @RequestMapping("/employees")
+    String showEmployeesInfo(Model model) {
+        model.addAttribute("employees", employeeRepository.findAll());
+        return "employees_info";
+    }
+}
