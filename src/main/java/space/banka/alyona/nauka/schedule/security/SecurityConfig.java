a6b16639ac5b6ec4932c04d761ce0ebee5d9ce6e
Alyona Kovalyova <alyona+github@banka.space>
2020-05-25 19:52:31 +0300

    Add Spring Security

---
+++ src/main/java/space/banka/alyona/nauka/schedule/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package space.banka.alyona.nauka.schedule.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    interface Roles {
+        String DEPARTMENTS_ADMIN = "DepartmentsAdmin";
+        String EMPLOYEES_ADMIN = "EmployeesAdmin";
+        String TIMESHEET_MANAGER = "TimesheetManager";
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails departmentsAdmin = User.withDefaultPasswordEncoder()
+                .username("DepartmentsAdmin")
+                .password("password")
+                .roles(Roles.DEPARTMENTS_ADMIN)
+                .build();
+
+        UserDetails employeesAdmin = User.withDefaultPasswordEncoder()
+                .username("EmployeesAdmin")
+                .password("password")
+                .roles(Roles.EMPLOYEES_ADMIN)
+                .build();
+
+        UserDetails timesheetManager = User.withDefaultPasswordEncoder()
+                .username("TimesheetManager")
+                .password("password")
+                .roles(Roles.TIMESHEET_MANAGER)
+                .build();
+
+        return new InMemoryUserDetailsManager(departmentsAdmin, employeesAdmin, timesheetManager);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/departments").hasRole(Roles.DEPARTMENTS_ADMIN)
+                .antMatchers("/employees").hasRole(Roles.EMPLOYEES_ADMIN)
+                .antMatchers("/departments/*/timesheet").hasRole(Roles.TIMESHEET_MANAGER);
+        http.formLogin().permitAll();
+        http.logout().permitAll();
+    }
+}
