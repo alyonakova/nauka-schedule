bf57ff82fe3041e6ca516e7d3d4e224b3a542019
Alyona Kovalyova <alyona+github@banka.space>
2020-05-27 15:41:25 +0300

    Add method for finding by employee id and day id

--- src/main/java/space/banka/alyona/nauka/schedule/db/crud/EmployeeDayRepository.java
+++ src/main/java/space/banka/alyona/nauka/schedule/db/crud/EmployeeDayRepository.java
@@ -5,12 +5,13 @@ import space.banka.alyona.nauka.schedule.db.entities.Day;
 import space.banka.alyona.nauka.schedule.db.entities.EmployeeDay;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeDayRepository extends CrudRepository<EmployeeDay, EmployeeDay.Id> {
 
     Iterable<EmployeeDay> findByEmployeeId(Integer employeeId);
 
-    EmployeeDay findByEmployeeIdAndDayId(Integer employeeId, Day.Id dayId);
+    Optional<EmployeeDay> findByEmployeeIdAndDayId(Integer employeeId, Day.Id dayId);
 
     List<EmployeeDay> findByEmployeeIdAndDayIdYearAndDayIdMonth(Integer employeeId, Integer yearId, Integer monthId);
 
