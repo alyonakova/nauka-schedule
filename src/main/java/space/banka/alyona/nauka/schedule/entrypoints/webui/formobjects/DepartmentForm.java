87e4c363b069d95ca65bc933882e33690ac7003d
Alyona Kovalyova <alyona+github@banka.space>
2020-05-30 19:13:10 +0300

    Create a form object

---
+++ src/main/java/space/banka/alyona/nauka/schedule/entrypoints/webui/formobjects/DepartmentForm.java
@@ -0,0 +1,14 @@
+package space.banka.alyona.nauka.schedule.entrypoints.webui.formobjects;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DepartmentForm {
+    String name;
+}
