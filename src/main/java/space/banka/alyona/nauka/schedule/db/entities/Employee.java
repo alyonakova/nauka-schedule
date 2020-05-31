718c8deb6231abe4d3a0c9894c95a4ae0b6b0796
Alyona Kovalyova <alyona+github@banka.space>
2020-05-31 22:24:13 +0300

    Make id a generated value

--- src/main/java/space/banka/alyona/nauka/schedule/db/entities/Employee.java
+++ src/main/java/space/banka/alyona/nauka/schedule/db/entities/Employee.java
@@ -4,20 +4,17 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Collection;
-import java.util.Date;
 
 @Entity
 @Data
 public class Employee {
 
     @Id
+    @GeneratedValue
     Integer id;
 
     String name;
